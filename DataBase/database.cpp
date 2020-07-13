16222f2e04b4c3acafe79cd9b9fb045f6426945c
Nelxon21 <67791591+Nelxon21@users.noreply.github.com>
2020-07-13 14:33:10 +0300

    Add files via upload

--- DataBase/database.cpp
+++ DataBase/database.cpp
@@ -1,4 +1,5 @@
 #include "database.h"
+#include<QFile>
 void Base::DataBase::DatabaseSetter() // метод для тэстов(будет вырезан, когда появится метод для чтения из файла)
 {
     for (int i =0; i<5; i++)
@@ -34,4 +35,56 @@ void Base::DataBase::Create(QString userName, QString userPassword)
     ContainerUserData.userName=userName;
     ContainerUserData.userPassword=userPassword;
     Data.push_back(ContainerUserData);
+}
+
+void Base::DataBase::SaveDatabase(QString file, QString cryptoKey)
+{
+        QFile f(file);
+        f.write((cryptoKey + "\n").toStdString().c_str());
+        for(userData el : Data) f.write(Crypto(el.userName) + " " + Crypto(el.userPassword) + "\n"); // Crypto - функция шифрования, принимает QString
+        f.close();
+}
+
+
+void Base::DataBase::LoadDatabase(QString file, QString cryptoKey)
+{
+    QFile f(file);
+    QStringList lst;
+    if(f.open(QIODevice::ReadOnly |QIODevice::Text))
+        {
+            while(!f.atEnd())
+            {
+                QString str = f.readLine();
+                lst = str.split(" ");
+            }
+
+        }
+    this->cryptoKey = lst[0];
+    for(int i=1;i<lst.size();i++)
+    {
+        int symbol;
+        for(int j=0;j<lst[i].length();j++)
+        {
+            if(lst[i][j] == " ")
+            {
+                symbol = j;
+                break;
+            }
+        }
+        QString name = " ";
+        QString pass = " ";
+        for(int j=0;j<symbol;j++)
+        {
+            name += lst[i][j];
+        }
+        for(int j = symbol+1;j<lst[i].length();j++)
+        {
+            pass += lst[i][j];
+        }
+        userData d1;
+        d1.userName = Decrypt(name);
+        d1.userPassword = Decrypt(pass);
+        Data.push_back(d1);
+        f.close();
+    }
 }
