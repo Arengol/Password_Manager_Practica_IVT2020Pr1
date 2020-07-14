7462a49118b2e855d3d5ae72980057a435626699
Kupuh <67791244+Kupuh@users.noreply.github.com>
2020-07-14 17:35:24 +0300

    Update passwordgenerator.h

--- PasswordGenerator/passwordgenerator.h
+++ PasswordGenerator/passwordgenerator.h
@@ -1,10 +1,24 @@
 #ifndef PASSWORDGENERATOR_H
 #define PASSWORDGENERATOR_H
 
-class PasswordGenerator
+#include <ctime>
+#include <string>
+#include <fstream>
+#include <iostream>
+using namespace std;
+
+class CPassGen
 {
+private:
+    string pass;
+    int length;
+
 public:
-    PasswordGenerator();
+    void setLength(int p_length);
+    void setNumber(int p_number);
+    CPassGen();//конструктор по умолчанию
+    string getPassword(int pointerPass);//возвращает пароль длинной length
 };
+
 
 #endif // PASSWORDGENERATOR_H
