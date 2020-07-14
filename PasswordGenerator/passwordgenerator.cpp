4ad1c3011e35d1b755fda24c635c86f2fbd0df3c
Kupuh <67791244+Kupuh@users.noreply.github.com>
2020-07-14 17:34:30 +0300

    Update passwordgenerator.cpp

--- PasswordGenerator/passwordgenerator.cpp
+++ PasswordGenerator/passwordgenerator.cpp
@@ -1,5 +1,34 @@
 #include "passwordgenerator.h"
 
-PasswordGenerator::PasswordGenerator()
+CPassGen::CPassGen()
 {
+    srand(time(0));//установка начального значения генератора случайных чисел
 }
+
+void CPassGen::setLength(int p_length)
+{
+    length = p_length;
+}
+
+string CPassGen::getPassword(int pointerPass)
+{
+    string password = "";
+    switch (pointerPass)
+    {
+        case 1:
+        for (int i = 0; i < length; i++)
+            password += char('0' + rand() % ('9' - '0'));
+        break;
+        case 2:
+        for (int i = 0; i < length; i++)
+            password += char('a' + rand() % ('z' - 'a'));
+        break;
+        case 3:
+        for (int i = 0; i < length; i++)
+            password += char('0' + rand() %('z' - '0'));
+        break;
+
+    }
+    return password;
+}
+
