124aea31c456eed215424dbc244992c84cbe8722
Arengol <67644124+Arengol@users.noreply.github.com>
2020-07-15 12:07:44 +0300

    Update crypto.h

--- Crypto/crypto.h
+++ Crypto/crypto.h
@@ -1,10 +1,19 @@
-#ifndef CRYPTO_H
-#define CRYPTO_H
+#ifndef CRYPT_H
+#define CRYPT_H
 
+
+#include <QString>
+#include <vector>
 class Crypto
 {
+    int table [2][94];
+
 public:
-    Crypto();
+    Crypto(QString startKey);
+    QString Crypt(QString text);
+    QString Decrypt(QString text);
+    void mixer();
+    ~Crypto();
 };
 
-#endif // CRYPTO_H
+#endif // CRYPT_H
