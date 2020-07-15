d8d7e16c2000f5f925d0f49939379cd21a03eda6
Arengol <67644124+Arengol@users.noreply.github.com>
2020-07-15 12:27:55 +0300

    Update crypto.h

--- Crypto/crypto.h
+++ Crypto/crypto.h
@@ -1,12 +1,12 @@
 #ifndef CRYPTO_H
 #define CRYPTO_H
-#include <QString>
-#include <vector>
+
 #include <QString>
 #include <vector>
 class Crypto
 {
     int table [2][94];
+
 public:
     Crypto(QString startKey);
     QString Crypt(QString text);
@@ -17,3 +17,4 @@ public:
 
 
 #endif // CRYPTO_H
+
